84ef2d2a8c3115ad8697bc6844fcee47b2a8abac
Anurag Bhandari <anurag.bhd@gmail.com>
2019-07-25 16:06:59 +0530

    Convert CoursePage into a container component
    Courses page was doing more than it should. First, it's designed to
    display a list of courses. In addition to that, it was also handling
    creation of new courses. Second, it's wired to Redux so it should not
    have much JSX.
    
    To convert to container component
    * Remove all add course JSX
    * Remove all event handlers related to add course

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,39 +5,14 @@ import { bindActionCreators } from "redux";
 import * as courseActions from "../../redux/actions/courseActions";
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <div>
+      <>
         <h2>Courses</h2>
-        <form onSubmit={this.handleSubmit}>
-          <h3>Add Course</h3>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            value={this.state.course.title}
-          />
-          <input type="submit" value="Save" />
-        </form>
         {this.props.courses.map(course => {
           return <div key={course.title}>{course.title}</div>;
         })}
-      </div>
+      </>
     );
   }
 }
