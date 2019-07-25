b02e370f697b9a5884a610252a90c91f8afdf1fe
Anurag Bhandari <anurag.bhd@gmail.com>
2019-07-25 16:18:53 +0530

    Add load courses action as a thunk
    Remember, thunks help in dealing with async data.

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,19 @@
 import * as types from "./actionTypes";
+import * as courseAPI from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    courseAPI
+      .getCourses()
+      .then(courses => {
+        dispatch({ type: types.LOAD_COURSES_SUCCESS, courses });
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
