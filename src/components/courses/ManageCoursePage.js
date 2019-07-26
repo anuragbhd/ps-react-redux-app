859d3b0a5ccc53b558d5ceba66e1c33d63fad93b
Anurag Bhandari <anurag.bhd@gmail.com>
2019-07-26 22:02:30 +0530

    Fix a bug in manage course page
    Opening an existing course directly from url wasn't working.
    This was because course state was getting set only once,
    which was before courses state could be loaded.

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -23,6 +23,8 @@ function ManageCoursePage({
       loadCourses().catch(error => {
         alert(`Loading courses failed: ${error}`);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -30,7 +32,7 @@ function ManageCoursePage({
         alert(`Loading authors failed: ${error}`);
       });
     }
-  }, []); // empty array indicates useEffect will execute only once when component mounts
+  }, [props.course]); // empty array indicates useEffect will execute only once when component mounts
 
   function handleChange(event) {
     const { name, value } = event.target;
