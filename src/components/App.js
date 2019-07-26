5fb9023bd113c7d477790319c23134fc0786f421
Anurag Bhandari <anurag.bhd@gmail.com>
2019-07-26 17:05:22 +0530

    Add a new page to manage courses
    * Add a new functional (container) component ManageCoursePage
    * Add relevant routes in App.js
    
    With this component, we try something new. As noted in the videos,
    functional components have several benefits and should be preferred
    over class components.
    
    Using CoursesPage as our starting point, we have converted it from
    class to function. Then we added the useEffect hook to let us simulate
    componentDidMount() lifecycle method. (side)effect = lifecycle method.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from "./common/Header";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 import PageNotFound from "./PageNotFound";
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route path="/about" component={AboutPage} />
         <Route component={PageNotFound} />
       </Switch>
